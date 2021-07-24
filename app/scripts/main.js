ceafc36e92142e01839adf4ae912678b8a961cd5
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2021-07-24 12:59:30 -0300

    feat: more products

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,44 @@
 const request = new XMLHttpRequest();
+let nextPage;
 
-request.open("GET", "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1");
+function getProducts(url) {
+  request.open("GET", `https://${url}`);
 
-request.onload = function () {
-  const { products } = JSON.parse(this.responseText);
+  request.onload = function () {
+    const result = JSON.parse(this.responseText);
+    nextPage = result.nextPage;
 
-  const container = document.getElementById("products");
+    const container = document.getElementById("products");
 
-  products.forEach((result, idx) => {
-    const product = document.createElement("div");
-    product.classList = "product-body";
+    result.products.forEach((result, idx) => {
+      const product = document.createElement("div");
+      product.classList = "product-body";
 
-    const content = `
-      <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
-        <div class="product">  
-          <h5>${result.name}</h5>
-          <p >${result.description}</p>
-          <p>De: ${result.oldPrice}</p>
-          <p>Por: ${result.price}</p>
-          <button class="send" type="button">Comprar</button>
+      const content = `
+        <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
+          <div class="product">  
+            <h5>${result.name}</h5>
+            <p >${result.description}</p>
+            <p>De: ${result.oldPrice}</p>
+            <p>Por: ${result.price}</p>
+            <button class="send" type="button">Comprar</button>
+          </div>
         </div>
-      </div>
-    `;
+      `;
 
-    container.innerHTML += content;
-  });
-};
+      container.innerHTML += content;
+    });
+  };
 
-request.onerror = function () {
-  alert("Request error!");
-};
+  request.onerror = function () {
+    alert("Request error!");
+  };
 
-request.send();
+  request.send();
+}
+
+function changePage() {
+  getProducts(nextPage);
+}
+
+getProducts("frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1");
