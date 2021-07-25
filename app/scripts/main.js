b3257f30432b1877c54a89445e1fd8ddb183314a
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2021-07-25 13:57:15 -0300

    feat: check input data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -43,4 +43,52 @@ function changePage() {
   getProducts(nextPage);
 }
 
+function emailIsValid(email) {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+function cpfIsValid(cpf) {
+  const regex = /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{11}$)/;
+  return regex.test(String(cpf).toLowerCase());
+}
+
+function register() {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const cpf = document.getElementById("cpf");
+  const male = document.getElementById("male");
+  const female = document.getElementById("female");
+
+  if (!emailIsValid(email.value)) {
+    email.style.borderColor = "red";
+  } else {
+    email.style.removeProperty("border");
+  }
+  if (!cpfIsValid(cpf.value)) {
+    cpf.style.borderColor = "red";
+  } else {
+    cpf.style.removeProperty("border");
+  }
+
+  const user = {
+    name: name.value,
+    email: email.value,
+    cpf: cpf.value,
+    male: male.checked,
+    female: female.checked,
+  };
+  /* Could send user by post */
+}
+
+function shareNew() {
+  const email = document.getElementById("femail");
+  if (!emailIsValid(email.value)) {
+    email.style.borderColor = "red";
+  } else {
+    email.style.removeProperty("border");
+  }
+}
+
 getProducts("frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1");
